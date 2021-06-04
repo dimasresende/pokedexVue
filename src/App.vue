b21e13c92b9c31cbd53baf5ba9736cbd051488e8
Dimas Resende <dresende@uolinc.com>
2021-06-04 20:11:53 -0300

    Implementando filtro de busca simples

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
       <h4 class="is-size-4">Pokedex Simples em Vue</h4>
-      <div v-for="(pokemon, index) in pokemons" :key="index">
+      <input class="input is-rounded" type="text" placeholder="Buscar Pokemon pelo nome" v-model="busca">
+      <button id="btnBusca" class="button is-fullwidth is-success" @click="buscar">Buscar</button>
+      <div v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url">
         <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1" />
       </div>
     </div>
@@ -17,27 +19,52 @@ export default {
   name: 'App',
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function() {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
   },
   components: {
     Pokemon
+  },
+  methods: {
+    buscar: function() {
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' ') {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  computed: {
+    // resultadoBusca: function() {
+    //   if(this.busca == '' || this.busca == ' ') {
+    //     return this.pokemons;
+    //   } else {
+    //     return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+    //   }
+    // }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  #btnBusca {
+    margin-top: 2%;
+  }
 </style>
